d3104cb28bf02b56567902d2eea8c24d5daecd99
patsicko <patsicko@g>
2022-09-11 13:29:39 +0200

    dashboard

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./sidebar.scss"
+
+const Sidebar = () => {
+  return (
+
+    <div className='sidebar'>
+        <div className="top">
+            <span className="logo">Patrick Admin</span>
+        </div>
+        <div className="center">
+       
+         <ul>
+            <li>
+                <span>Dashboard</span>
+            </li>
+            <li>
+                <span>Dashboard</span>
+            </li>
+            <li>
+                <span>Dashboard</span>
+            </li>
+            <li>
+                <span>Dashboard</span>
+            </li>
+         </ul>
+        </div>
+        <div className="bottom">color options</div>
+        
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./home.scss";
+import Sidebar from '../../components/sidebar/Sidebar';
+
 
 const Home = () => {
   return (
     <div className='home'>
         Home
+        <Sidebar/>
     </div>
   )
 }
